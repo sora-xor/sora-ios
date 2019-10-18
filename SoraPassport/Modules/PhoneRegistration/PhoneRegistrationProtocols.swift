f144cea3012cc6034b1b8c5b976a9b2febe13bc2
Russel <emkil.russel@gmail.com>
2019-10-18 16:42:30 +0300

    license add

--- SoraPassport/Modules/PhoneRegistration/PhoneRegistrationProtocols.swift
+++ SoraPassport/Modules/PhoneRegistration/PhoneRegistrationProtocols.swift
@@ -1,3 +1,8 @@
+/**
+* Copyright Soramitsu Co., Ltd. All Rights Reserved.
+* SPDX-License-Identifier: Apache 2.0
+*/
+
 protocol PhoneRegistrationViewProtocol: ControllerBackedProtocol, LoadableViewProtocol {
     func didReceive(viewModel: PersonalInfoViewModelProtocol)
 }
